449da40619009cff34cb9356e4a6211aea77f83d
ngailapdi <ngailapdi@gmail.com>
2016-10-12 13:41:53 -0400

    User, UserList and AccountType added

--- src/model/RegisteredUsersList.java
+++ src/model/RegisteredUsersList.java
@@ -1,7 +1,13 @@
 package model;
 
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by simkieu on 10/11/16.
  */
 public class RegisteredUsersList {
+    static List<User> databaseUserList = new ArrayList<User>();
 }
